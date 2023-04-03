73d072fcebd9bbcf1513e36660fcb998f5ea84f4
Saif Mahmud <>
2023-04-03 08:27:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,70 @@
+# Day - 2 Challenge
+print("Getting to know you! \n")
+
+
+Name = input("Please, enter your Name : ")
+favoriteFood = input("Please, tell me your favorite food item : ")
+favoriteMusic = input("Please, What kind of music you like : ")
+livePlace = input("Where do you live : ")
+
+
+print("You are")
+print(Name)
+print("You're probabaly hungry for")
+print(favoriteFood)
+print("and you're definitely getting your groove on to\nMy man,", favoriteMusic)
+print("living in the amazing")
+print(livePlace)
+print ("Have a great day!")
+
+
+
+'''
+myName = input("Enter your Name :- ")
+myAge = input("Enter your Age :- ")
+replit = input("What do you think :- ")
+
+
+print()
+print("So, You are\n",myName,"\nand the ripe old age of \n",myAge,"\nand clearly think that Replit is \n",replit)
+
+print()
+print("So, You are")
+print(myName)
+print("and are the ripe old age of")
+print(myAge)
+print("and clearly think that Replit is")
+print(replit)
+
+
+#fix my code
+myVariable = input("WHO GOES THERE? ")
+print(myVariable)
+
+myGrandma = input("How's your Grandma doing? 😘 ")
+print(myGrandma)
+
+myLunch = input("What are you having for lunch? ")
+
+print("Hmm, it sounds like you really should just order")
+print(myLunch)
+print("as soon as possible!")
+
+print("Definitely not a Phishing Scam")
+print()
+myName = input("Your Name =
+")
+print("Thanks for logging in")
+print(myName)
+cardNumber = input("What is your credit card number? ")
+print("Thanks, I definitely wont put")
+print(cardNumber)
+print("into Amazon and order anything weird")
+print()
+print("Promise")
+maidenName = input("What is your Mother's maiden name? ")
+print()
+print("That's cool, I just wanted to know that")
+print(maidenName)
+print("was your Mum's maiden name. Not because the bank requested it or anything, honest.")
+'''
